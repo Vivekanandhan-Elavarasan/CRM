bfb03abca613e253c623cf4174eb6816cc4df202
Vivekanandhan-Elavarasan <63054867+Vivekanandhan-Elavarasan@users.noreply.github.com>
2020-07-22 15:19:04 +0530

    fifth commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+let http = require('http').Server(app);
 app.use(bodyParser.json());
 
 const uri = `mongodb+srv://vivekuser:vivekadmin@cluster0-mfrcr.mongodb.net/shorturl02?retryWrites=true&w=majority`;
@@ -84,7 +85,53 @@ app.post("/login", function (req, res) {
     }    
 });
 
+app.post('/create', function (req, res, next) {
+    client.connect(err => {
+      const customers = client.db("crmdb").collection("customers");
+      
+      let customer = { name: req.body.name, address: req.body.address, telephone: req.body.telephone, note: req.body.note };
+      customers.insertOne(customer, function(err, res) {
+        if (err) throw err;
+        console.log("1 customer inserted");
+      });
+    })
+    res.send('Customer created');
+  })
 
+  app.get('/get-client', function (req, res) {
+    client.connect(err => {
+        client.db("crmdb").collection("customers").findOne({name: req.query.name}, function(err, result) {
+          if (err) throw err;
+          res.render('update', {oldname: result.name, oldaddress: result.address, oldtelephone: result.telephone, oldnote: result.note, name: result.name, address: result.address, telephone: result.telephone, note: result.note});
+        });
+      });
+});
+
+
+app.post('/update', function(req, res) {
+    client.connect(err => {
+      if (err) throw err;
+      let query = { name: req.body.oldname, address: req.body.oldaddress, telephone: req.body.oldtelephone, note: req.body.oldnote };
+      let newvalues = { $set: {name: req.body.name, address: req.body.address, telephone: req.body.telephone, note: req.body.note } };
+      client.db("crmdb").collection("customers").updateOne(query, newvalues, function(err, result) {
+          if (err) throw err;
+          console.log("1 document updated");
+          res.render('update', {message: 'Customer updated!', oldname: req.body.name, oldaddress: req.body.address, oldtelephone: req.body.telephone, oldnote: req.body.note, name: req.body.name, address: req.body.address, telephone: req.body.telephone, note: req.body.note});
+        });
+    });
+  })
+  
+  app.post('/delete', function(req, res) {
+    client.connect(err => {
+      if (err) throw err;
+      let query = { name: req.body.name, address: req.body.address ? req.body.address : null, telephone: req.body.telephone ? req.body.telephone : null, note: req.body.note ? req.body.note : null };
+      client.db("crmdb").collection("customers").deleteOne(query, function(err, obj) {
+        if (err) throw err;
+        console.log("1 document deleted");
+        res.send(`Customer ${req.body.name} deleted`);
+      });
+    });
+  })
 
 app.listen(port, () => {
     console.log("app listing in port " + port);
