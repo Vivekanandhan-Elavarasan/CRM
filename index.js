022de08fef20b9ece12ebdf8c41ae56c5df4548e
Vivekanandhan-Elavarasan <63054867+Vivekanandhan-Elavarasan@users.noreply.github.com>
2020-07-22 12:41:32 +0530

    second commit

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ app.use(bodyParser.json());
 
 const uri = `mongodb+srv://vivekuser:vivekadmin@cluster0-mfrcr.mongodb.net/shorturl02?retryWrites=true&w=majority`;
 
-app.post("/register",function(req,res) {
+
+
+
+app.post("/register", function (req, res) {
     var email = req.body.email;
     var fname = req.body.fname;
     var lname = req.body.lname;
@@ -15,13 +18,30 @@ app.post("/register",function(req,res) {
     console.log(req.body);
 
     const client = new MongoClient(uri, { useNewUrlParser: true });
-    client.connect(function(err,db){
-        if(err) throw err;
+
+    client.connect(function (err, db) {
+        if (err) throw err;
+
         var dbObject = db.db("crmdb");
-       
 
+        dbObject.collection("register").find({ email: email }).toArray(function (err, data) {
+            if (err) throw err;
+            if (data.length > 0) {
+                console.log("Present");
+                res.status(400).send("Email already Registered !");
+            }
+            else {
+                console.log("Not Present");
+                bcrypt.hash(pass, 10, function (err, hash) {
+                    var testObj = { email: email, pass: hash, fname:fname,lname:lname,type:type ,role: "User" };
+                    console.log(testObj);
+                    dbObject.collection("register").insertOne(testObj, function (err, resp) {
+                        if (err) throw err;
+                        db.close();
+                    });
+                });
+            }
+        })
     });
-
-
 });
 
